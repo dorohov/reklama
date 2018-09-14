f691efdd5b0cc2a28c2a2cedf6b11da2493129b0
caezar47 <iss47@ya.ru>
2018-09-14 11:08:47 +0300

    2018-09-

--- src/block/site/default/audio/.document-ready.js
+++ src/block/site/default/audio/.document-ready.js
@@ -48,5 +48,53 @@ $('#close').click(function(e) {
 	$('#pause').replaceWith('<a class="button gradient" id="play" href="" title=""></a>');
 	$('#close').fadeOut(300);
 });
-*/
+*/ 
+$('.js--audio-play, .js--audio-mute').on('click', function () {
+	// Меняем иконку
+	/*var glyphicon = $(this).find('.glyphicon');
+	var toggleClass = glyphicon.data('toggle-class');
+	glyphicon.data('toggle-class', glyphicon.attr('class')).removeClass().addClass(toggleClass);*/
 
+	var audio = $(this).closest('.js--audio-wrap').find('.js--audio-cont');
+	var timeline = audio.closest('.js--audio-wrap').find('.js--timeline');
+	var duration = audio.prop('duration');
+	var width = timeline.width();
+	if ($(this).hasClass('js--audio-play')) {
+		// Старт/пауза и двигаем ползунок
+		if(audio.prop('paused')) {
+			audio.trigger('play');
+			var idInterval = setInterval(function () {
+				var currentTime = audio.prop('currentTime');
+				var left = width*currentTime/duration;
+				timeline.find('.js--timeline-control').css('left', left+'px');
+				timeline.find('.js--timeline-progress').css('width', left+'px');
+				if (currentTime == duration) {
+					clearInterval(idInterval);
+				}
+			}, 1000);
+		} else {
+			audio.trigger('pause');
+			clearInterval(idInterval);
+		}
+	} else {
+		// Переключаем звук
+		audio.prop("muted",!audio.prop("muted"));
+	}
+	return false;
+});
+
+/**
+ * Перемотка трека по клику на timeline
+ */
+$('.js--timeline').on('click', function (e) {
+	var audioTime = $(this).closest('.js--audio-wrap').find('.js--audio-cont');
+	var duration = audioTime.prop('duration');
+	if (duration > 0) {
+		var offset = $(this).offset();
+		var left = e.clientX-offset.left;
+		var width = $(this).width();
+		$(this).find('.js--timeline-control').css('left', left+'px');
+		audioTime.prop('currentTime', duration*left/width);
+	}
+	return false;
+});
